784cba5d68d8cdba5cd713937d681e7afd0393c7
thesilv3r <thesilv3r@users.noreply.github.com>
2014-09-21 16:03:16 +1000

    Update cachematrix.R
    Fixed some typos and expanded comments around what the code is doing.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
 ## The overall intent of these combined functions is to reduce computational
-## stress in calculating the invere of a matrix multiple times via the use
+## stress in calculating the inverse of a matrix multiple times via the use
 ## of cached objects.
 
 makeCacheMatrix <- function(baseMtx = matrix(), ...) {
-	## Creates a special matrix object that is able to cache its inverse by
-	## creating a list to
+	## Creates a special matrix object that is able to cache its inverse
+	## by creating a list to
 	## 1. Set the value of an input matrix
 	## 2. Get the value of the given matrix
 	## 3. Set the value of the inverse of the matrix given
@@ -15,14 +15,16 @@ makeCacheMatrix <- function(baseMtx = matrix(), ...) {
 	set <- function(y) {
 		baseMtx <<- y
 		mtxInverse <<- NULL
-		}
+		} ## in global environment sets defined matrix as input 
+		  ## and clears any cached inverse
 	
-	get <- function() baseMtx
+	get <- function() baseMtx ## a function to retrieve the input matrix
 
-	setinverse <-    function(solve)
-			 mtxInverse <<- solve
+	setinverse <- function(solve) mtxInverse <<- solve ##calculates inverse based
+                                                           ## on an argument called "solve"	
 
-	getinverse <- function() mtxInverse
+	getinverse <- function() mtxInverse ## defined function to return the value of
+					    ## an already cached matrix inverse
 
 	list(set = set, get = get, 
 	     setinverse = setinverse, 
@@ -30,18 +32,20 @@ makeCacheMatrix <- function(baseMtx = matrix(), ...) {
 }
 
 cacheSolve <- function(baseMtx, ...) {
-	## Calculate inverse of defined Matrix, if
-	## already exists, return from cache
+	## Calculate inverse of defined Matrix, if it already exists, return from cache
 
-	mtxInverse <- baseMtx$getinverse()
+	mtxInverse <- baseMtx$getinverse() ## set value based on getinverse function defined when
+					   ## running makeCacheMatrix
 
 	if(!is.null(mtxInverse)) {
 		message("Retrieving data from cache")
 		return(mtxInverse)
-	}
+	} ## checks if the object returned from getinverse exists (i.e. if it has previously been 
+	  ## calculated). If so, it will return this value, avoiding re-calculation, if not the function
+	  ## continues to execute
 
-	data <- baseMtx$get()
-	mtxInverse <- solve(data, ...)
-	baseMtx$setinverse(mtxInverse)
-	mtxInverse
+	data <- baseMtx$get() ## retrives base matrix
+	mtxInverse <- solve(data, ...) ## calculates the inverse of the base matrix
+	baseMtx$setinverse(mtxInverse) ## sets the calculated inverse into the cache for later recall 
+	mtxInverse ## prints calculated inverse
 }
